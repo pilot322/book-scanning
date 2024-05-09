9e5d9f27d09731ec1b38e23faf294f093180c068
Mixalhs Lappas <michalis.lappas@outlook.com>
2024-05-09 16:55:54 +0300

    removed .env from frontend because vercel i guess

--- book-tracker/src/api/axios.js
+++ book-tracker/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
+
 
 const BASE_URL = 'https://68f2fcb3-d5f9-42a1-bfcf-ebcafe366e6d-00-1ji9fwriai6lm.kirk.replit.dev/';
 const TEST_URL = 'http://localhost:4000/';
@@ -9,7 +8,7 @@ const TEST_URL = 'http://localhost:4000/';
 
 
 
-const TESTING_ENVIRONMENT = process.env.TESTING;
+const TESTING_ENVIRONMENT = false;
 
 export default axios.create({
     baseURL: TESTING_ENVIRONMENT ? TEST_URL : BASE_URL
