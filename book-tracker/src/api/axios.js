28045b3046f70871630626f8d369b149b9d44a80
Mixalhs Lappas <michalis.lappas@outlook.com>
2024-05-09 16:53:09 +0300

    added .env to frontend

--- book-tracker/src/api/axios.js
+++ book-tracker/src/api/axios.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const BASE_URL = 'https://68f2fcb3-d5f9-42a1-bfcf-ebcafe366e6d-00-1ji9fwriai6lm.kirk.replit.dev/';
 const TEST_URL = 'http://localhost:4000/';
 
-const TESTING_ENVIRONMENT = true;
+
+
+
+const TESTING_ENVIRONMENT = process.env.TESTING;
 
 export default axios.create({
     baseURL: TESTING_ENVIRONMENT ? TEST_URL : BASE_URL
